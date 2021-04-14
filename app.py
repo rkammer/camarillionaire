ce94a0f2de24bb1676f364ea57952bc525f163c7
Rodrigo <rkammer@missionproduce.com>
2021-04-14 09:41:52 -0700

    latest

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import json
 load_dotenv()
 
 #back to og
-t.update_status(
-    w.get_formated_forecast()
-)
+# t.update_status(
+#     w.get_formated_forecast()
+# )
 
-# v.speak(w.get_formated_forecast())
+v.speak("My Brother, the lecture of the EA. degree is divided into three sections. The first, being a recapitulation of the ceremonies there which you have just passed, will be omitted. The second, rationally explains why you were caused to submit to the various forms and ceremonies made use of during your initiation.")
--- helpers/voice.py
+++ helpers/voice.py
@@ -1,8 +1,25 @@
 import playsound
 from gtts import gTTS
+from gtts.tokenizer import pre_processors
+import gtts
 
 def speak(text) :
-    tts = gTTS(text = text, lang="en")
+
+    gtts.tokenizer.symbols.SUB_PAIRS.append(
+        ('EA.', 'Entered Apprendice')
+    )
+
+    tts = gTTS(
+        text = text,
+        lang="en",
+        pre_processor_funcs=[
+            pre_processors.tone_marks,
+            pre_processors.end_of_line,
+            pre_processors.abbreviations,
+            pre_processors.word_sub
+        ]
+    )
+
     filename = "./audios/voice.mp3"
     tts.save(filename)
     playsound.playsound(filename)
